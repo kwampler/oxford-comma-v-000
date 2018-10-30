7ba4ccabe4949c463f5af1435aa733cdc7d17d77
katejsenger <katejsenger@yahoo.com>
2018-10-30 00:03:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
   elsif array.length == 1
   	array.join
   elsif array.length == 3
-    array.join (", " " and ")
+    array.join (", ")
+    array.join (" and ")
   end
 end
